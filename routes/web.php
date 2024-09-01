e4212f3a99ac7731cfa809eb8ca948d4be20d276
4arnab <axmedarnab3@gmail.com>
2024-09-01 08:27:07 +0530

    basic laravel routing

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,29 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
+// All Routes, Middlewares, Views and more;
 Route::get('/', function () {
-    return view('welcome');
+    return view('welcome',[
+        'jobs'=>[
+            "name"=>"ahmed",
+            "arnab"=>"google",
+        ]
+    ]);
+});
+
+Route::get('/hello', function(){
+    return response("Hello world")
+    ->header("Content-Type","text/plain")
+    ->header("foo",'bar');
+});
+
+Route::get('/posts/{id}', function($id){
+    // ddd($id);
+    return response("post $id");
+})->where('id','[0-9]+');
+
+Route::get('/search', function(Request $request){
+    return $request['name'].$request['age'];
 });
